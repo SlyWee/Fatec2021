5a3510604834b0681b9814f779e3068da0b5dcea
SlyWee <73496199+SlyWee@users.noreply.github.com>
2021-03-22 01:55:37 -0300

    Add files via upload

---
+++ Aula6GITHUB/src/java/MesServlet.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author easypc
+ */
+public class MesServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet MesServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            out.println("<center><h3>");
+            
+            int Mes = Integer.parseInt(request.getParameter("mes"));
+            out.println("Mês: " + Mes);
+            int Ano = Integer.parseInt(request.getParameter("ano"));
+            out.println("Ano: " + Ano);
+
+            out.println("</h3></center>");
+            
+            Calendar calendario = new GregorianCalendar(2021, Mes-1, 1);
+            int dias = calendario.getActualMaximum(Calendar.DAY_OF_MONTH);
+            
+            out.println("<table border=1 bgcolor=\"#8CE4BE\" align=center>");
+            
+            
+            
+            for (int i = 1; i <= dias; i++){
+                if (i % 7 == 0) out.println("<tr>");
+                out.println("<td align=center height=100 width=100><h3>" + i + "</h3></td>");
+            }
+            
+            out.println("</table>");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
